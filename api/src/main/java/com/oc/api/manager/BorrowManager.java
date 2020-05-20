b86728a7a49cebba4b135567a76142496f24d4cf
ccathala <cathala.cha@gmail.com>
2020-05-20 16:30:25 +0200

    Add method to lock borrow extension + Test

--- api/src/main/java/com/oc/api/manager/BorrowManager.java
+++ api/src/main/java/com/oc/api/manager/BorrowManager.java
@@ -1,6 +1,7 @@
 package com.oc.api.manager;
 
 import com.oc.api.model.beans.Borrow;
+import com.oc.api.web.exceptions.FunctionnalException;
 
 import java.util.List;
 import java.util.Optional;
@@ -9,7 +10,7 @@ public interface BorrowManager {
 
     List<Borrow> getAllBorrows();
     Optional<Borrow> getById(int id);
-    Borrow save(Borrow borrow, String operationType);
+    Borrow save(Borrow borrow, String operationType) throws FunctionnalException;
     void deleteById(int id);
     List<Borrow> getAllBorrowsByRegistereduserId(int userId);
     List<Borrow> getAllBorrowsByBookIdAndLibraryId(int book_id, int library_id);
